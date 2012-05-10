fa0e762d1164ed00c3da8deb48929bb83e2e99f8
Thomas <thomassennekamp@online.de>
2012-05-10 14:27:31 +0200

    gehts jetzt

---
+++ templates_c/993d0a8df898c0f4072435541bc301f02265491a.file.test.tpl.php
@@ -0,0 +1,27 @@
+<?php /* Smarty version Smarty-3.1.8, created on 2012-05-10 14:02:22
+         compiled from "tpl\test.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:144214fabae3cb89540-10942232%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '993d0a8df898c0f4072435541bc301f02265491a' => 
+    array (
+      0 => 'tpl\\test.tpl',
+      1 => 1336651340,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '144214fabae3cb89540-10942232',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.8',
+  'unifunc' => 'content_4fabae3cbce422_66353641',
+  'variables' => 
+  array (
+    'test' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_4fabae3cbce422_66353641')) {function content_4fabae3cbce422_66353641($_smarty_tpl) {?><?php echo $_smarty_tpl->tpl_vars['test']->value;?>
+<?php }} ?>
