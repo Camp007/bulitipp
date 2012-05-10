740284c49edd4e1bec6e663217c7b2f33f5f89c9
Thomas <thomassennekamp@online.de>
2012-05-10 14:26:37 +0200

    qaw

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
+require_once 'libs/smarty/Smarty.class.php';
+require_once 'database.php';
+$smarty = new Smarty();
 
-echo "test";
-<ydf
-asdf
-asdf
-asdf
-asfda
-sdf
+$smarty->assign( "test", "was auch immer " );
+$smarty->display( 'tpl/test.tpl' );
+
+$db = new Database();
+$d = $db->create();
+
+$sql = "SELECT * FROM test";
+$res = $d->query( $sql );
+while( $ds = $res->fetch_assoc(  ) ){
+	echo $ds['test'];
+}
+
+
 
 ?>
+asd
