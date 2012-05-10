094267fe33f744899697b5dacdf4529c0588b2e9
Pascal <info@html-projekt.de>
2012-05-10 20:34:31 +0200

    Alles vorbereitet

--- index.php
+++ index.php
@@ -3,6 +3,42 @@ require_once 'libs/smarty/Smarty.class.php';
 require_once 'classes/Database.php';
 $smarty = new Smarty();
 
+$d = $_GET['d']; $s = $_GET['s']; $g = $_GET['g'];
+
+
+
+if( $d == '' ){
+	
+
+	$smarty->assign( "startseite",  "Startseite" );
+	$content = $smarty->fetch( 'tpl/startseite.tpl' );
+	
+	
+}elseif( $d == 'tipps' ){
+
+	$smarty->assign( "tipps", "Tipps" );
+	$content = $smarty->fetch( 'tpl/tipps.tpl' );
+	
+	/*
+	$cat = new CategoryService;
+
+	$ent = new EntrieService;
+	$ar = $ent->getEntByCat( $d );
+	$smarty->assign( 'News',  $ar );
+	$content .= $smarty->fetch( 'tpl/uebersicht.tpl' );
+	
+*/
+	
+	
+}elseif( $d != '' AND $s != '' ){
+	
+	
+}
+
+	$smarty->assign( 'header', $header );	
+	$smarty->assign( 'content', $content );
+	$smarty->display( 'tpl/index.tpl' );	
+
 
 
 ?>
