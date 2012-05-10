e7cbe35c468ce46c3a74371c6be635dd5fa4b3f1
Pascal <info@html-projekt.de>
2012-05-10 20:06:38 +0200

    asd

--- index.php
+++ index.php
@@ -1,37 +1,8 @@
 <?php
 require_once 'libs/smarty/Smarty.class.php';
-require_once 'database.php';
+require_once 'classes/Database.php';
 $smarty = new Smarty();
-// MEIN TESt2
-error_reporting( E_ERROR | E_WARNING | E_PARSE );
-if( $_GET['d']  == '' ){
-	$user = new TipperService;
-	$ar = $user->getAllUser();
-	$header = Header::getHeader();
-	$test = TipperService::getTest();
-	$smarty->assign( "mannschaften", $ar );
-}elseif( $_GET['d'] == 'detail' ) {
-
-}
 
 
 
-/*
-$smarty->assign( "allew", $content );
-$smarty->display( 'tpl/test.tpl' );
-
-$db = new Database();
-$d = $db->create();
-
-$sql = "SELECT * FROM test";
-$res = $d->query( $sql );
-while( $ds = $res->fetch_assoc(  ) ){
-	echo $ds['test'];
-}
-
-
-*/
-$smarty->assign( "header", $header );
-$smarty->assign( "allew", $content );
-$smarty->display( 'tpl/test.tpl' );
 ?>
