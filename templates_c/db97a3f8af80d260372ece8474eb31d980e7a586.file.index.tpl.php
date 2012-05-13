e80c6c25536252d5413db15ecf1751a538047581
Sebastian Essling <sebi.essling@googlemail.com>
2012-05-13 16:28:45 +0200

    Kommentar

--- templates_c/db97a3f8af80d260372ece8474eb31d980e7a586.file.index.tpl.php
+++ templates_c/db97a3f8af80d260372ece8474eb31d980e7a586.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.8, created on 2012-05-10 20:30:59
+<?php /* Smarty version Smarty-3.1.8, created on 2012-05-13 16:19:10
          compiled from "tpl\index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:2354fac0963838c05-74292291%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'db97a3f8af80d260372ece8474eb31d980e7a586' => 
     array (
       0 => 'tpl\\index.tpl',
-      1 => 1336674658,
+      1 => 1336918569,
       2 => 'file',
     ),
   ),
@@ -15,14 +15,14 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.8',
+  'unifunc' => 'content_4fac09639029b9_68995266',
   'variables' => 
   array (
     'header' => 0,
     'content' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.8',
-  'unifunc' => 'content_4fac09639029b9_68995266',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_4fac09639029b9_68995266')) {function content_4fac09639029b9_68995266($_smarty_tpl) {?>
 
